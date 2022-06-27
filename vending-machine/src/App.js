e8b0d4907c4cce540cdb02b387cfa018ff599cfd
Ryan <ryjen.pears@gmail.com>
2022-06-27 13:59:53 +0200

    updated app with router

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import VendingMachine from './VendingMachine';
+import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <VendingMachine />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
